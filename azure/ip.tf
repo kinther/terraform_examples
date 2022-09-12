88a8a0580159747a1d4345396d3a65e2d1b3252b
kinther <xdelaranx@gmail.com>
2022-09-12 10:09:47 -0700

    Create ip.tf

---
+++ azure/ip.tf
@@ -0,0 +1,16 @@
+resource "azurerm_public_ip" "example_ip" {
+    name                        = "example_public_ip"
+    location                    = azurerm_resource_group.rg.location
+    resource_group_name         = azurerm_resource_group.rg.name
+    allocation_method           = "Static"
+    sku                         = "Standard"
+    zones                       = ["1"]
+}
+
+resource "azurerm_public_ip_prefix" "example_ip_prefix" {
+    name                        = "example_public_ip_prefix"
+    location                    = azurerm_resource_group.rg.location
+    resource_group_name         = azurerm_resource_group.rg.name
+    prefix_length               = 30
+    zones                       = ["1"]
+}
