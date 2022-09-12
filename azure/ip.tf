2217d1be9775d77f130927e44ca2b1859524184b
kinther <xdelaranx@gmail.com>
2022-09-12 10:11:23 -0700

    Update ip.tf

--- azure/ip.tf
+++ azure/ip.tf
@@ -11,6 +11,6 @@ resource "azurerm_public_ip_prefix" "example_ip_prefix" {
     name                        = "example_public_ip_prefix"
     location                    = azurerm_resource_group.rg.location
     resource_group_name         = azurerm_resource_group.rg.name
-    prefix_length               = 30
+    prefix_length               = 32
     zones                       = ["1"]
 }
